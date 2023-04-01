331274ac9be7e77b2cab77ae1f22353f51c65e69
Jesuloba <jesulobajohn@gmail.com>
2023-04-01 18:13:58 +0100

    layout and loader setup

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,102 @@
+import { useState, useEffect, FC, ReactNode } from "react";
+import styled, { ThemeProvider } from "styled-components";
+// import { Head, Loader, Nav, Social, Email, Footer } from "@components";
+import { Loader } from "@/components";
+import { GlobalStyle, theme } from "@/styles";
+import { StyledContent } from "./layout.style";
+import { useRouter } from "next/router";
+
+interface layoutProps {
+	children: ReactNode;
+}
+
+const Layout: FC<layoutProps> = ({ children }) => {
+	const router = useRouter();
+	const isHome = router.pathname === "/";
+	const [isLoading, setIsLoading] = useState(isHome);
+
+	// Sets target="_blank" rel="noopener noreferrer" on external links
+	const handleExternalLinks = () => {
+		const allLinks = Array.from(document.querySelectorAll("a"));
+		if (allLinks.length > 0) {
+			allLinks.forEach((link) => {
+				if (link.host !== window.location.host) {
+					link.setAttribute("rel", "noopener noreferrer");
+					link.setAttribute("target", "_blank");
+				}
+			});
+		}
+	};
+
+	const UseHash: (str: string) => void = (str) => {
+		console.log(str);
+		if (!str) {
+			const hash = str.split("#")[1];
+			console.log(hash);
+			setTimeout(() => {
+				const el = document.getElementById(hash);
+				if (el) {
+					el.scrollIntoView();
+					el.focus();
+				}
+			}, 0);
+		}
+		return;
+	};
+
+	useEffect(() => {
+		if (isLoading) {
+			return;
+		}
+
+		const onWindowHashChange = () => UseHash(window.location.hash);
+		const onNextJSHashChange: (url: string) => void = (url) => UseHash(url);
+
+		router.events.on("hashChangeStart", onNextJSHashChange);
+		window.addEventListener("hashchange", onWindowHashChange);
+		window.addEventListener("load", onWindowHashChange);
+
+		handleExternalLinks();
+
+		return () => {
+			router.events.off("hashChangeStart", onNextJSHashChange);
+			window.removeEventListener("load", onWindowHashChange);
+			window.removeEventListener("hashchange", onWindowHashChange);
+		};
+	}, [isLoading, router.events]);
+
+	return (
+		<>
+			{/* <Head /> */}
+
+			<div id="root">
+				<ThemeProvider theme={theme}>
+					<GlobalStyle />
+
+					<a className="skip-to-content" href="#content">
+						Skip to Content
+					</a>
+
+					{isLoading && isHome ? (
+						<>
+							<Loader finishLoading={() => setIsLoading(false)} />
+						</>
+					) : (
+						<StyledContent>
+							{/* <Nav isHome={isHome} /> */}
+							{/* <Social isHome={isHome} /> */}
+							{/* <Email isHome={isHome} /> */}
+
+							<div id="content">
+								{children}
+								{/* <Footer /> */}
+							</div>
+						</StyledContent>
+					)}
+				</ThemeProvider>
+			</div>
+		</>
+	);
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,5 @@ import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "@/styles";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+	return <Component {...pageProps} />;
 }
