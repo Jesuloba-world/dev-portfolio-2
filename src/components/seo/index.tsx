fbfd28ce33df80cbe24e0cfa20508834de79d97e
Jesuloba <jesulobajohn@gmail.com>
2023-05-07 04:20:13 +0100

    added google verification meta tag

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -46,10 +46,10 @@ const Head: FC<headProps> = ({ title, description, image }) => {
 			<meta name="twitter:description" content={seo.description} />
 			<meta name="twitter:image" content={seo.image} />
 
-			{/* <meta
+			<meta
 				name="google-site-verification"
-				content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk"
-			/> */}
+				content="eQSIffyw13icguPew6YxwD4ApPwSGwf254SpSDUb_hY"
+			/>
 		</SEO>
 	);
 };
