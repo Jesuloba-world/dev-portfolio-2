d8af2e32e5bc08de1c0ec1c2b03c2762f31131de
Jesuloba <jesulobajohn@gmail.com>
2023-05-17 14:08:21 +0100

    change to hero text

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
 
 	const one = <h1>Hi, my name is</h1>;
 	const two = <h2 className="big-heading">Jesuloba Abere.</h2>;
-	const three = <h3 className="big-heading">I build software solutions.</h3>;
+	const three = <h3 className="big-heading">I turn ideas into software.</h3>;
 	const four = (
 		<>
 			<p>
