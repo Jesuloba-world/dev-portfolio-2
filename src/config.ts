db0efcf117dc18bd24360528dd2868f172f7ad32
Jesuloba <jesulobajohn@gmail.com>
2023-07-24 09:45:51 +0100

    small fix

--- src/config.ts
+++ src/config.ts
@@ -76,7 +76,7 @@ export const srConfig = (delay = 200, viewFactor = 0.25) => ({
 export const siteMetaData = {
 	defaultTitle: "Jesuloba Abere | Software Engineer",
 	defaultDescription:
-		"Software engineer specializing in techniques that optimize user experience",
+		"Software engineer specialized in techniques that optimize user experience",
 	siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
 	defaultImage: "images/seo_image.png",
 	twitterUsername: "@jesuloba0_",
